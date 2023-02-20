7c6a70b6021ecc3893dfea9dc774d8cac38e8d99
Serkan Uslubas <serkan@uslubas.de>
2023-02-20 01:46:17 +0100

    installed packages

--- DF.Webinar.GraphQL.DatabaseUp/Program.cs
+++ DF.Webinar.GraphQL.DatabaseUp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DF.Webinar.GraphQL.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+string ConnectionString = "Server=(localdb)\\mssqllocaldb;Database=graphqldb-local;Trusted_Connection=Yes;";
+
+using IHost host = Host.CreateDefaultBuilder(args)
+.ConfigureServices(services =>
+{
+    services.AddDbContextFactory<DfContext>(opt =>
+    {
+        opt.UseSqlServer(ConnectionString);
+        opt.EnableSensitiveDataLogging();
+    });
+})
+.Build();
+
+
+
+await host.RunAsync();
