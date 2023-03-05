6f90d18db7b0450b5f26ea109b896e300edddcf9
Serkan Uslubas <serkan@uslubas.de>
2023-03-05 13:42:18 +0100

    Filter

--- 02-End/DF.Webinar.GrahpQL.Frontend/Program.cs
+++ 02-End/DF.Webinar.GrahpQL.Frontend/Program.cs
@@ -2,6 +2,7 @@ using DF.Webinar.GrahpQL.Frontend;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using StrawberryShake;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
